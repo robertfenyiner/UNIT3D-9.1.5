338e5bc34b7b16a3d7912d2151f65055bb3c12a4
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2025-08-10 11:33:16 -0500

    Remove debug messages - git:update command ready for production

--- app/Console/Commands/GitUpdater.php
+++ app/Console/Commands/GitUpdater.php
@@ -107,11 +107,6 @@ class GitUpdater extends Command
     final public function handle(): void
     {
         $this->io = new SymfonyStyle($this->input, $this->output);
-
-        // DEBUG: Show all available options
-        $this->info('DEBUG: Available options: ' . json_encode(array_keys($this->options())));
-        $this->info('DEBUG: Has skip-backup: ' . ($this->hasOption('skip-backup') ? 'true' : 'false'));
-        $this->info('DEBUG: Has yes: ' . ($this->hasOption('yes') ? 'true' : 'false'));
         
         $this->logFile = storage_path('logs/git-updater-'.now()->format('Y-m-d').'.log');
         $this->log('Starting GitUpdater');
