f5010ad7c5dbe95803b3e4cbeb92934d7ade0665
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2025-09-19 12:59:15 -0500

    Fix Rotten Tomatoes

--- resources/views/torrent/partials/movie-meta.blade.php
+++ resources/views/torrent/partials/movie-meta.blade.php
@@ -198,7 +198,7 @@
                 <a
                     class="meta-id-tag"
                     {{-- cspell:disable-next-line --}}
-                    href="https://html.duckduckgo.com/html/?q={{ $meta->title ?? '' }}  ({{ substr($meta->release_date ?? '', 0, 4) ?? '' }})+site%3Arottentomatoes.com"
+                    href="https://www.rottentomatoes.com/search?search={{ urlencode($meta->title ?? '') }}%20{{ substr($meta->release_date ?? '', 0, 4) ?? '' }}"
                     title="Rotten Tomatoes: {{ $meta->title ?? '' }}  ({{ substr($meta->release_date ?? '', 0, 4) ?? '' }})"
                     target="_blank"
                     rel="noreferrer"
